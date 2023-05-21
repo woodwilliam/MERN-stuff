3f99d09cc14cc2af2b76c2dd43dc7a37e7124e7a
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-05-21 17:07:34 -0500

    added type defsbased of readme descriptions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,46 @@
-//ADD CODE
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+}
+type Query {
+    getSingleUser: User
+}
+
+type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(body: BookInput!): User
+    deleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
